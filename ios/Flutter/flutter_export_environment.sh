af702587598f93258dba2094432a95cac4349fdd
shrikarthik007 <holebagilshrikarthik@gmail.com>
2022-05-27 14:32:29 +0530

    get packeges

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Karthik\Flutter Project\bitcoin-ticker-flutter-master"
+export "FLUTTER_ROOT=C:\flutter\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\Flutter Projects\bitcoin_ticker"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
